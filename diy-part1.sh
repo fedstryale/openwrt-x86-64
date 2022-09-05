462cacd54a44ec59c9bba3c770617cd9bceb1bb1
fedstryale <fedstryale@gmail.com>
2022-09-05 10:53:50 +0800

    add small

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,3 +21,4 @@ sed -i '/^[^#]/ s/\(^.*telephony.*$\)/#\1/' -i feeds.conf.default
 
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
